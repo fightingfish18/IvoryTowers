0d1fffdb800f6fabc58d75af3cd70b1b6ae84017
Ali Haugh <haughar@hotmail.com>
2015-12-08 20:37:59 -0800

    Add the rest of the view controller stuff back in.

--- IvoryTowers/ListViewController.swift
+++ IvoryTowers/ListViewController.swift
@@ -1,46 +1,13 @@
 //
-//  SecondViewController.swift
+//  ListViewController.swift
 //  IvoryTowers
 //
-//  Created by Cechi Shi on 12/3/15.
+//  Created by Alison Rose Haugh on 12/8/15.
 //  Copyright © 2015 University of Washington. All rights reserved.
 //
 
 import UIKit
 
-class ListViewController: UITableViewController {
-    /* let locationNames : [String] = []
-       let avgRatings : [Double] = []
-       var query : String?
-    */
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+class ListViewController: UIViewController {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return locationNames.count
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("someCellName", forIndexPath: indexPath) as! QuizCell
-        cell.name.text = locationNames[indexPath.row]
-        switch avgRatings[index.row] {
-        *** tiered images based on rating to the nearest half-star ***
-        }
-        return cell
-    }*/
 }
-
