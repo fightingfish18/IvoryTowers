677814e9035e219628cadfee5a953848d1d21ec3
Ali Haugh <haughar@hotmail.com>
2015-12-08 19:32:45 -0800

    Add detail view controller.

---
+++ IvoryTowers/LocationDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationDetailViewController.swift
+//  IvoryTowers
+//
+//  Created by Alison Rose Haugh on 12/8/15.
+//  Copyright © 2015 University of Washington. All rights reserved.
+//
+
+import Cocoa
+
+class LocationDetailViewController: UIViewController {
+
+}
