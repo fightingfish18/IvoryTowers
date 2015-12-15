51a3a0209dafa69f4530dc349886b9d606f9897b
Nathan Booco <nbooco@uw.edu>
2015-12-14 20:52:07 -0800

    Working Parse signup

--- IvoryTowers/SignInViewController.swift
+++ IvoryTowers/SignInViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class SignInViewController: UIViewController {
     @IBOutlet weak var loginName: UITextField!
     @IBOutlet weak var loginPassword: UITextField!
+    @IBOutlet weak var signupName: UITextField!
+    @IBOutlet weak var signupPass: UITextField!
+    @IBOutlet weak var signupConfirm: UITextField!
     
     
     override func viewDidLoad() {
@@ -24,4 +27,35 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func logIn(sender: AnyObject) {
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        if (signupName.text != nil) && (signupPass.text != nil) && (signupConfirm.text != nil) {
+            if (signupPass.text == signupConfirm.text) {
+                var user = PFUser()
+                user.username = signupName.text
+                user.password = signupPass.text
+                user.signUpInBackgroundWithBlock{
+                    (succeeded: Bool, error: NSError?) -> Void in
+                    if let error = error {
+                        let errorString = error.userInfo["error"] as? NSString
+                        let alertController = UIAlertController(title: "User already exists", message: "That username is taken.", preferredStyle: UIAlertControllerStyle.Alert)
+                        alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
+                        self.presentViewController(alertController, animated: true, completion: nil)
+                    } else {
+                    // Hooray! Let them use the app now.
+                    }
+                }
+            } else {
+                let alertController = UIAlertController(title: "Mismatch", message: "Your password and password confirmation must match.", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        } else {
+            let alertController = UIAlertController(title: "Blank Fields", message: "All three fields are required.", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
 }
